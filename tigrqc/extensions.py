dec7e1258b646973f398aa503d04cb1f65f87508
Dawn Smith <dawn.smith@camh.ca>
2026-03-16 17:59:42 -0400

    [ENH] Add flask's login manager

--- tigrqc/extensions.py
+++ tigrqc/extensions.py
@@ -4,15 +4,31 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
 if TYPE_CHECKING:
     from flask import Flask
 
+    from tigrqc.models import User
+
 db = SQLAlchemy()
+lm = LoginManager()
 
 
 def init_extensions(app: Flask) -> None:
     """Initialize all extensions.
     """
     db.init_app(app)
+
+    lm.init_app(app)
+
+    @lm.user_loader
+    def load_user(uid: str) -> User | None:
+        # lazy import to avoid circular imports
+        # pylint: disable=import-outside-toplevel
+        from tigrqc.models import User
+        try:
+            return User.get(int(uid))
+        except ValueError:
+            return None
