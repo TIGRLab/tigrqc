90873c9ec6f993f06f48c97f42ed4b97ac316c77
Dawn Smith <dawn.smith@camh.ca>
2026-03-27 22:08:35 -0400

    [CHORE] Add urls, make version dynamic
    Add __about__.py to manage things like version and copyright

---
+++ tigrqc/__about__.py
@@ -0,0 +1,8 @@
+"""Manage information about the package.
+
+This holds package information that is better off not being hard-coded in
+pyproject.toml (whether because the app needs easy access to the info or
+because it's just not packaging-related).
+"""
+__version__ = '0.0.1'
+__copyright__ = '2026 Kimel Family Translational Imaging-Genetics Research Lab'
