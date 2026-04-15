181b06b3f7616838b6800229da562dc2b2e7e4ab
Dawn Smith <dawn.smith@camh.ca>
2026-04-14 20:56:37 -0400

    [ENH] Allow auth to be toggled at start up for routes.
    - Adds two anon user classes depending on whether auth is
      used.
    - Adds a decorator to exempt routes from login requirement
    - Adds a check of whether to enable/disable based on user config

---
+++ tigrqc/access.py
@@ -0,0 +1,121 @@
+"""Controls authentication access to routes.
+
+By default all routes require the user to log in before accessing them,
+unless the route has specifically been marked as 'public'.
+
+If authentication is deliberately disabled by the user's configuration then
+every route will be accessible without requiring a user to log in. When running
+in this mode the anonymous user is considered to have admin-level permissions.
+This mode should **never be run in production**.
+"""
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from flask import abort, redirect, request, session, url_for
+from flask_login import AnonymousUserMixin
+
+from tigrqc.exceptions import ConfigException
+from tigrqc.interfaces import BaseUser
+
+if TYPE_CHECKING:
+    from flask import Flask
+    from flask_login import LoginManager
+
+
+class AnonymousUser(BaseUser, AnonymousUserMixin):
+    """Represent a 'user' that has not logged in.
+    """
+    is_admin = False
+
+
+class NoAuthAnonymousUser(BaseUser, AnonymousUserMixin):
+    """Represent a 'user' when authentication is disabled.
+    """
+    is_admin = True
+
+
+def auth_disabled(app: Flask) -> bool:
+    """Check if authentication is disabled for this app instance.
+
+    Authentication is considered disabled when either:
+        - The user has deliberately set the environment variable to disable it.
+        - The app is using debug mode and no authentication methods have
+            been configured.
+
+    Raises:
+        ConfigException: When no authentication methods have been defined and
+            the app is running as a production instance and authentication
+            hasn't been deliberately disabled.
+    """
+    if app.config.get('AUTH_DISABLED'):
+        return True
+
+    debug = app.config.get('DEBUG', False)
+    methods_exist = bool(app.config.get('AUTH_METHODS'))
+
+    if not debug and not methods_exist:
+        raise ConfigException(
+            'When running outside of debug mode at least one authentication '
+            'method must be defined for the app to run safely. Alternatively '
+            'authentication may be intentionally disabled (NOT RECOMMENDED).'
+        )
+
+    return debug and not methods_exist
+
+
+def public_route(f):
+    """A decorator that marks a route as 'public'.
+
+    Routes that use this decorator will _not_ require the user to log in
+    to access them.
+    """
+    f.is_public = True
+    return f
+
+
+def require_login_globally(app: Flask):
+    """Protect every route behind a requirement to log in first.
+
+    This should be run after all routes have been added to the app instance.
+
+    The 'static' folder and routes marked with the 'no_login' decorator will
+    be exempt. If the user has requested authentication to be disabled this
+    will do nothing.
+    """
+    if auth_disabled(app):
+        return
+
+    @app.before_request
+    def require_login():
+        """Before each request ensure the user is logged in, if not public.
+        """
+        if request.endpoint == 'static':
+            # Static files should always be accessible
+            return None
+
+        view = app.view_functions.get(request.endpoint)
+        if getattr(view, 'is_public', False):
+            # Views marked public with the decorator should be accessible
+            return None
+
+        if 'user_id' not in session:
+            if request.accept_mimetypes.accept_json:
+                # API routes should receive 401 rather than redirect
+                abort(401)
+            return redirect(url_for('user.login', next=request.path))
+
+        return None
+
+
+def set_anon_user(app: Flask, lm: LoginManager):
+    """Add the correct AnonymousUser to the login manager.
+
+    If authentication is enabled the AnonymousUser should have extremely
+    limited access and permissions. If it's disabled the AnonymousUser needs
+    global access and max permissions because it's the only user.
+    """
+    if auth_disabled(app):
+        lm.anonymous_user = NoAuthAnonymousUser
+        return
+    lm.anonymous_user = AnonymousUser
