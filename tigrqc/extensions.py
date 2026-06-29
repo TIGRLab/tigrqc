1afeaa8abde92c6fe7a4089318591bb8d076715f
Dawn Smith <dawn.smith@camh.ca>
2026-06-29 14:46:36 -0400

    [ENH] Turn on app-wide csrf protection by default

--- tigrqc/extensions.py
+++ tigrqc/extensions.py
@@ -6,6 +6,7 @@ from typing import TYPE_CHECKING
 
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 from sqlalchemy.orm import DeclarativeBase
 
 from tigrqc.access import set_anon_user
@@ -21,6 +22,7 @@ class Base(DeclarativeBase):
     """
 
 
+csrf = CSRFProtect()
 db = SQLAlchemy(model_class=Base)
 enc = FernetEncryption()
 lm = LoginManager()
@@ -29,6 +31,7 @@ lm = LoginManager()
 def init_extensions(app: Flask) -> None:
     """Initialize all extensions.
     """
+    csrf.init_app(app)
     db.init_app(app)
     enc.init_app(app)
 
