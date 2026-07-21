a7024a40d300fe253b85800829dec857192a16b2
Dawn Smith <dawn.smith@camh.ca>
2026-07-21 18:56:30 -0400

    [FIX] Ensure sqlite instances enforce foreign key constraints

--- tigrqc/extensions.py
+++ tigrqc/extensions.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import CSRFProtect
+from sqlalchemy import event
 from sqlalchemy.orm import DeclarativeBase
 
 from tigrqc.access import set_anon_user
@@ -22,6 +23,26 @@ class Base(DeclarativeBase):
     """
 
 
+def register_sqlite_foreign_key_enforcement(
+        app: Flask, app_db: SQLAlchemy
+) -> None:
+    """Turn on foreign key enforcement for sqlite connections.
+
+    By default, for some legacy reasons, sqlite databases don't actually
+    pay attention to foreign key constraints. This will ensure every sqlite
+    connection turns on foreign key constraints.
+    """
+    with app.app_context():
+        if app_db.engine.dialect.name != 'sqlite':
+            return
+
+        @event.listens_for(app_db.engine, 'connect')
+        def set_sqlite_foreign_keys_on(dbapi_connection, _):
+            cursor = dbapi_connection.cursor()
+            cursor.execute('PRAGMA foreign_keys=ON')
+            cursor.close()
+
+
 csrf = CSRFProtect()
 db = SQLAlchemy(model_class=Base)
 enc = FernetEncryption()
@@ -32,7 +53,10 @@ def init_extensions(app: Flask) -> None:
     """Initialize all extensions.
     """
     csrf.init_app(app)
+
     db.init_app(app)
+    register_sqlite_foreign_key_enforcement(app, db)
+
     enc.init_app(app)
 
     set_anon_user(app, lm)
