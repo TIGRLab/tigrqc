82a9c4c4b121798414dcd327a265354acad72513
Dawn Smith <dawn.smith@camh.ca>
2026-08-10 17:03:14 -0400

    [ENH] Update handling of irregular bids subject dirs
    - Process sess dirs even when some invalid items present
    - Process subs without optional sess dirs
    - Remove some no longer needed code and comments

--- tigrqc/load_datasets.py
+++ tigrqc/load_datasets.py
@@ -9,11 +9,11 @@ from abc import ABC
 from pathlib import Path
 
 from tigrqc.exceptions import TigrQcException
-# from tigrqc.models import InvalidDataDir
+from tigrqc.models import Timepoint, Attempt, Series, TimepointDir, File, InvalidData
 from tigrqc.models import get_or_create
 
 # To do:
-#   - Implement overrides of templates + patterns (but must constraint it to
+#   - Implement overrides of templates + patterns (but must constrain it to
 #       avoid user incident... How to be flexible without matching all or
 #       none by accident?)
 #   - Verify these match all intended + none of unintended per name scheme
@@ -273,190 +273,6 @@ def read_dataset(
 
 #     return
 
-from tigrqc.models import Timepoint, Attempt, Series, TimepointDir, File, InvalidData
-def read_raw_bids(dataset_path: Path, dataset_id: int, study_id: str, bc: BidsConvention):
-    """A test function read in a bids dataset of raw data.
-    """
-    # Should maybe have handling here in case the dataset path no longer
-    # exists when the read is run (like moved or archived or deleted since
-    # first added to DB)
-
-    # Replace input args with a dataset / datadir (refs to args must update)
-    # Figure out where / how to add site validation
-
-    # Dataset path is a directory of bids folders in this case
-    for item in dataset_path.iterdir():
-        if not item.is_dir():
-            continue
-
-        # May want to handle exceptions here
-        # Site and study code validation should happen here if anywhere, to
-        # avoid wasting time parsing misnamed folders.
-        parsed_id = bc.parse_id(item.name)
-
-        if not parsed_id:
-            # Update table or re-add to model
-            # bad_subdir = InvalidDataDir(
-            #     dataset_id=dataset_id,
-            #     subdir=item.name
-            # )
-
-            # Need to update this to reflect a dataset is now a collection
-            # of directories
-            bad_subdir = InvalidData(
-                sourcedir_id=dataset_id,
-                rel_path=item.name,
-            )
-            # May want to handle exceptions here
-            bad_subdir.save()
-            continue
-
-        # Try to make database records here
-        #   info needed:
-        #       - Study code if it exists (will have foreign key on study anyway)
-        #       - Site if it exists. If ID doesn't include... and more than 1
-        #           site exists in the project, you're screwed.
-        #       - SUBID, will always exist
-        #       - timepoint (bids sess) num, will always exist
-        #       - session (repeat), if it exists will be in a sidecar. Otherwise set
-        #           to 1. Have to read all sidecars and separate into
-        #           sessions in case more than one exists at read time.
-        #       - Time the folder was made or updated
-        #       Further:
-        #           - One record per nifti found
-        #               -> type (from modality field) is this needed by the dash now?
-        #               -> series number
-        #               -> series description (needed?)
-        #               -> date it was made / modified (?) on file system
-        #               -> Date the scan was done according to scanner (?)
-        #               -> The json contents
-        #               -> Do we currently store bvec / bval? or do we just
-        #                   grab it when we 'know' from tags that it exists?
-        #                   is there a sensible way to hook it took the main record
-        #                   without having to do this tag inference etc.?
-
-
-        # Probably want to pull these fields out of the re object and
-        # normalize so you can just grab parsed_id.study, etc. without the whole dance
-        study = parsed_id.group('study') if 'study' in parsed_id.groupdict() else ''
-        site = parsed_id.group('site') if 'site' in parsed_id.groupdict() else ''
-        subid = parsed_id.group('id') if 'id' in parsed_id.groupdict() else parsed_id.group('subid')
-
-        if study and study != study_id:
-            # Failed study code validation, so report it.
-            bad_subdir = InvalidData(
-                sourcedir_id=dataset_id,
-                rel_path=item.name,
-            )
-            # May want to handle exceptions here
-            bad_subdir.save()
-            continue
-
-        # parsed_dir = {}
-        # for nifti in item.rglob('*.nii*'):
-        #     # parent.parent will either be sub- or ses- or a mistake
-        #     session_id = bc.regexes['session'].match(nifti.parent.parent.name)
-
-        #     if session_id:
-        #         session = session_id.group('timepoint')
-        #     elif nifti.parent.parent == item:
-        #         # No ses-, valid bids and means session = 1
-        #         session = '01'
-        #     else:
-        #         parsed_dir.setdefault('invalid', []).append(nifti)
-        #         continue
-
-        #     parsed_dir.setdefault(session, {}).setdefault(nifti.parent.name, []).append(nifti)
-
-        invalid_subdirs = []
-        for nifti in item.rglob('*.nii*'):
-            # Do you really want to check if session / timepoint exists every
-            # single time and add it or is there a better way here...
-            if nifti.parent.parent in invalid_subdirs:
-                # Skip items from already-known-to-be-wrong 'ses' dirs
-                continue
-
-            session_id = bc.regexes['session'].match(nifti.parent.parent.name)
-
-            if session_id:
-                session = session_id.groups('timepoint')
-            elif nifti.parent.parent == item:
-                # The optional ses- folder was omitted. So it's session 1
-                session = '01'
-            else:
-                # valid bids is either:
-                #       sub-X/ses-Y/modality/nifti
-                #       OR sub-X/modality/nifti
-                # At this point it's neither. So capture the bad parent folder
-                # and ignore its contents until it's handled.
-                invalid_subdirs.append(nifti.parent.parent)
-                continue
-
-            # Need somewhere to store this / translate to a 'type'
-            # Or can it be abandoned if dash isn't generating things?
-            # validation worth while?
-            modality = nifti.parent.name
-
-
-
-
-
-
-        # This is maybe a bad approach. Use above.
-        for session in item.iterdir():
-
-            if not session.is_dir():
-                continue
-
-            # This should probably be replaced with a method
-            # Also, ses folders are optional when only one session exists...
-            parsed_ses = bc.regexes['session'].match(session.name)
-
-            if not parsed_ses:
-                # In valid bids the subdirs should either be modality dirs
-                # OR a ses- dir. If not ses and no files found... I guess
-                # create an empty session and report it as empty.
-                timepoint = '01'
-            else:
-                # Clean this up too.
-                timepoint = parsed_ses.groups('timepoint') # e.g. '01'
-
-            # Ugh. Need modality too... Maybe just have a database overridable
-            # path parser. But then have to handle diff path separators.
-
-
-
-# Dataset:
-#   - Belongs to a project
-#   - Has 1+ source dirs
-#   - Has a type (raw data, qc metrics, pipeline data, etc.)
-#   - Aggregate the invalid and valid dirs? Later.
-
-# Source dir
-#   - Has a path
-#   - Has a name scheme (bids, nii, kcni)
-#   - Has valid subject dirs
-#   - Has invalid dirs
-
-# Subject dir
-#   - timepoint_id (what it represents)
-#   - sourcedir_id (what dir it belongs to)
-#   - dirname (The rel path for it within source dir)
-#   - Has valid files
-#   - Has invalid files
-
-# File
-#   - series_id (What it represents)
-#   - subdir_id (what dir it belongs to)
-#   - rel_path (the path for it within its dir)
-#   - file_type (how it can be used)
-#   - [future] contents? (i.e. for json and csv?)
-
-# InvalidData
-#   - sourcedir_id (The dir it belongs to)
-#   - timepoint_id (Optional, if unset it's invalid at subdir level otherwise nested)
-#   - rel_path (path to it within source)
-#   - ignore (whether to stop reporting it)
 
 from flask_sqlalchemy import SQLAlchemy
 def test_bids_load(
@@ -564,40 +380,32 @@ def test_bids_load(
                     (child, session_id.group('timepoint'))
                 )
 
-        if len(children['session']) > 0 and len(children['other']) > 0:
-            # Better option: Maybe parse the 'ses-' dirs and ignore the 'other'
-            # and report the 'other' so they can be fixed/ignored
-            #   -> This would still be invalid bids. But would prevent
-            #       the problem where a previously added correct sess
-            #       gets 'lost' because of a later added bad one?
-            #       is this even an issue though?
+        sessions = children['session']
+        other = children['other']
 
-            # Subject dir contains a mix of session and modality/other dirs
-            # report the issue. Attach the error to the subject dir
-            # since it affects multiple child dirs inside it.
-            get_or_create(
-                db.session,
-                InvalidData,
-                sourcedir_id=sourcedir_id,
-                rel_path=subject_dir.name,
-                other_fields={
-                    'error_msg': (
-                        'BIDS subject dir should contain only 1) session '
-                        'dirs or 2) modality dirs. Unknown directories found.'
-                    ),
-                },
-            )
-            continue
-
-        if len(children['other']) > 0:
-            # Set default session/num == '01'
-            # Make a timepoint for it.
-            # Validate the modalities present in the list, if possible
-            # then parse with a general 'bids session parser' func'
-            continue
-
-        for sess_dir, num in children['session']:
-            test_bids_sess_load(db, subid, num, sess_dir, source_path)
+        if sessions and other:
+            # This is an error state. Report the 'other' dirs and process
+            # sessions normally
+            for subdir in other:
+                get_or_create(
+                    db.session,
+                    InvalidData,
+                    sourcedir_id=sourcedir_id,
+                    rel_path=subdir.relative_to(source_path),
+                    other_fields={
+                        'error_msg': (
+                            'BIDS subject dir should contain only 1) session '
+                            'dirs or 2) modality dirs. Unknown directories found.'
+                        ),
+                    },
+                )
+            for sess_dir, num in sessions:
+                test_bids_sess_load(db, subid, num, sess_dir, source_path)
+        elif other:
+            test_bids_sess_load(db, subid, '01', subject_dir, source_path)
+        else:
+            for sess_dir, num in sessions:
+                test_bids_sess_load(db, subid, num, sess_dir, source_path)
 
 
 def strip_suffixes(path: Path) -> str:
