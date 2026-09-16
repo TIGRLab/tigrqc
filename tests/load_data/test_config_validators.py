643db88c169071c145224e01be36877db3ac05ab
Dawn Smith <dawn.smith@camh.ca>
2026-09-15 20:44:01 -0400

    [TEST] Add tests for NameScheme validator

--- tests/load_data/test_config_validators.py
+++ tests/load_data/test_config_validators.py
@@ -6,6 +6,7 @@ import typing
 
 import pytest
 from pydantic import BaseModel, ValidationError
+from pydantic_core import PydanticCustomError
 
 import tigrqc.load_data.config_validators as config
 import tigrqc.load_data.post_processors  # Import needed for monkeypatch
@@ -173,7 +174,7 @@ class TestBuildKwargsValidator:
         model()
 
 
-class TestStrictBaseModel:  # pylint: disable=too-few-public-methods
+class TestStrictBaseModel:
     """Tests for StrictBaseModel
 
     StrictBaseModel is a pydantic BaseModel that sets extra="forbid" by
@@ -192,6 +193,110 @@ class TestStrictBaseModel:  # pylint: disable=too-few-public-methods
 
         with pytest.raises(ValidationError):
             MyTestClass(a=1, b='hello')
+
+    def test_returns_pydantics_init_error_details(self):
+        """The output should be pydantic's InitErrorDetails.
+        """
+        result = config.StrictBaseModel.make_custom_error(
+            err_type='some_error',
+            err_msg='Something went wrong',
+            user_input='bad-value',
+        )
+
+        assert isinstance(result, dict)  # InitErrorDetails is a TypedDict
+        assert result['input'] == 'bad-value'
+        assert isinstance(result['type'], PydanticCustomError)
+
+    def test_loc_and_ctx_set_correct_default_values(self):
+        """Defaults for these options should match pydantic's expectations.
+        """
+        result = config.StrictBaseModel.make_custom_error(
+            err_type='some_error',
+            err_msg='Something went wrong',
+            user_input='bad-value',
+        )
+
+        assert result['loc'] == tuple()
+        assert result['ctx'] == {}
+
+    def test_provided_value_for_loc_gets_set_properly(self):
+        """When 'loc' is given it can be retrieved from output.
+        """
+        result = config.StrictBaseModel.make_custom_error(
+            err_type='some_error',
+            err_msg='Something went wrong',
+            user_input='bad-value',
+            loc=('fields', 'my_field'),
+        )
+
+        assert result['loc'] == ('fields', 'my_field')
+
+    def test_explicit_ctx_is_preserved_and_usable(self):
+        """When 'ctx' is given it can be retrieved from output.
+        """
+        ctx = {'name': 'foo'}
+        result = config.StrictBaseModel.make_custom_error(
+            err_type='some_error',
+            err_msg='Something went wrong',
+            user_input='foo',
+            ctx=ctx,
+        )
+
+        assert result['ctx'] == ctx
+
+    def test_message_renders_with_ctx_inserted_when_raised(self):
+        """When 'ctx' is given and err_msg contains a slot, ctx gets inserted.
+        """
+        error_detail = config.StrictBaseModel.make_custom_error(
+            err_type='some_error',
+            err_msg='Field {name} is invalid',
+            user_input='foo',
+            ctx={'name': 'foo'},
+        )
+
+        with pytest.raises(ValidationError) as exc_info:
+            raise ValidationError.from_exception_data(
+                title='Test',
+                line_errors=[error_detail],
+            )
+
+        assert 'Field foo is invalid' in str(exc_info.value)
+
+    def test_message_without_ctx_slots_renders_as_is_even_with_ctx(self):
+        """Message should be unaltered, even if ctx is given, if no slots.
+        """
+        error_detail = config.StrictBaseModel.make_custom_error(
+            err_type='some_error',
+            err_msg='Plain message with no slots',
+            user_input='x',
+            ctx={'name': 'foo'}
+        )
+
+        with pytest.raises(ValidationError) as exc_info:
+            raise ValidationError.from_exception_data(
+                title='Test',
+                line_errors=[error_detail],
+            )
+
+        assert 'Plain message with no slots' in str(exc_info.value)
+
+    def test_err_type_is_reflected_in_exception_error_type(self):
+        """The user provided 'err_type' should propagate to the exception.
+        """
+        error_detail = config.StrictBaseModel.make_custom_error(
+            err_type='my_custom_type',
+            err_msg='msg',
+            user_input='x',
+        )
+
+        with pytest.raises(ValidationError) as exc_info:
+            raise ValidationError.from_exception_data(
+                title='Test',
+                line_errors=[error_detail],
+            )
+
+        errors = exc_info.value.errors()
+        assert errors[0]['type'] == 'my_custom_type'
 
 
 class TestPostProcessorConfig:
@@ -318,3 +423,326 @@ class TestPostProcessorConfig:
         assert result.function is expected_func
         assert result.scope == 'series'
         assert result.args == args
+
+
+class TestNameSchemeHelpers:
+    """Tests for NameScheme's helper functions (create_field, etc.)
+    """
+    # pylint: disable=protected-access
+
+    def test_is_valid_group_name_correctly_finds_valid_names(self):
+        """Is valid group name should be able to identify valid names.
+        """
+        assert config.NameScheme._is_valid_group_name('valid_name1')
+        assert config.NameScheme._is_valid_group_name('MyGroupName')
+        assert not config.NameScheme._is_valid_group_name('1_num_cant_start')
+        assert not config.NameScheme._is_valid_group_name('has space')
+        assert not config.NameScheme._is_valid_group_name('has-dash')
+        assert not config.NameScheme._is_valid_group_name('')
+
+    def test_create_field_wraps_field_in_regex_group(self):
+        """Field is correctly wrapped in regex group.
+        """
+        group, error = config.NameScheme.create_field('subject', r'\d+')
+
+        assert group == r'(?P<subject>\d+)'
+        assert error is None
+
+    def test_create_field_reports_re_compile_issues(self):
+        """If re.compile has a problem it should produce an error message.
+        """
+        _, error = config.NameScheme.create_field('subject', '(')
+
+        assert error is not None
+        assert 'cannot compile' in error
+
+    def test_create_template_fills_template_and_wraps_in_group(self):
+        """A valid template should get filled in and wrapped in an re group.
+        """
+        fields = {'subject': r'(?P<subject>\d+)'}
+        template, error = config.NameScheme.create_template(
+            'sub_dir', 'sub-{subject}', fields
+        )
+
+        assert error is None
+        assert template == r'(?P<sub_dir>sub-(?P<subject>\d+))'
+
+    def test_create_template_reports_reference_to_unknown_field(self):
+        """A template can't reference an unknown field/template.
+        """
+        template, error = config.NameScheme.create_template(
+            'sub_dir', 'sub-{missing}', {}
+        )
+
+        assert template is None
+        assert 'non-existent field' in error
+
+    def test_report_invalid_group_names_returns_none_when_all_valid(self):
+        """If all group names are valid, the result should be None.
+        """
+        result = config.NameScheme._report_invalid_group_names(
+            {'good_name': 'also_good'}, check_vals=True
+        )
+
+        assert result is None
+
+    def test_report_invalid_group_names_reports_bad_keys(self):
+        """An invalid group name in the 'keys' should always be reported.
+        """
+        result = config.NameScheme._report_invalid_group_names(
+            {'Bad name': 'valid_name'}
+        )
+
+        assert 'Bad name' in result
+
+    def test_report_invalid_group_names_checks_keys_only_by_default(self):
+        """An invalid group name in values should be ignored by default.
+        """
+        result = config.NameScheme._report_invalid_group_names(
+            {'good_key': 'bad value'}
+        )
+
+        assert result is None
+
+    def test_report_invalid_group_names_checks_values_when_requested(self):
+        """If check_vals=True, should report invalid re groups in values too.
+        """
+        result = config.NameScheme._report_invalid_group_names(
+            {'good_key': 'bad value'}, check_vals=True
+        )
+
+        assert result is not None
+        assert 'bad value' in result
+
+
+class TestNameSchemeNameFormats:
+    """NameScheme tests of field/template/field_map name validity.
+    """
+
+    def test_invalid_field_name_raises_validation_error(self):
+        """A field name that isn't a valid regex group should raise.
+        """
+        with pytest.raises(ValidationError, match='Invalid entries'):
+            config.NameScheme(fields={'bad name': r'\d+'}, templates={})
+
+    def test_field_name_starting_with_number_is_invalid(self):
+        """Regex groups can't start with a number.
+        """
+        with pytest.raises(ValidationError, match='Invalid entries'):
+            config.NameScheme(fields={'1subject': r'\d+'}, templates={})
+
+    def test_invalid_field_map_key_raises_validation_error(self):
+        """The keys in field_map must be valid group names.
+        """
+        with pytest.raises(ValidationError, match='Invalid entries'):
+            config.NameScheme(
+                field_map={'bad key': 'sub'},
+                fields={'subject': r'\d+'},
+                templates={},
+            )
+
+    def test_invalid_field_map_value_raises_validation_error(self):
+        """The values in field_map must be valid group names.
+        """
+        with pytest.raises(ValidationError, match='Invalid entries'):
+            config.NameScheme(
+                field_map={'subject': 'bad value'},
+                fields={'subject': r'\d+'},
+                templates={},
+            )
+
+    def test_invalid_template_name_raises_validation_error(self):
+        """Template names must be a valid regex group name.
+        """
+        with pytest.raises(ValidationError, match='Invalid entries'):
+            config.NameScheme(
+                fields={'subject': r'\d+'},
+                templates={'bad name': 'sub-{subject}'},
+            )
+
+
+class TestNameScheme:
+    """Tests for core NameScheme functionality.
+    """
+
+    def test_exception_raised_when_required_config_missing(self):
+        """If a required config entry is absent, it should raise an exception.
+        """
+        with pytest.raises(ValidationError):
+            # fields missing
+            config.NameScheme(templates={})
+
+        with pytest.raises(ValidationError):
+            # templates missing
+            config.NameScheme(fields={})
+
+    def test_exception_raised_when_unknown_top_level_key_is_found(self):
+        """An unknown configuration key should raise an exception.
+        """
+        with pytest.raises(ValidationError):
+            config.NameScheme(fields={}, templates={}, whoopsy={})
+
+    def test_accepts_fields_only_w_empty_templates(self):
+        """Accepts config that contains fields and empty templates.
+        """
+        ns = config.NameScheme(
+            fields={'subject': r'\d+', 'session': r'\d+'},
+            templates={},
+        )
+
+        assert ns.scheme == {
+            'subject': r'(?P<subject>\d+)',
+            'session': r'(?P<session>\d+)',
+        }
+
+    def test_sets_defaults_when_field_map_and_post_processors_not_given(self):
+        """field_map and post_processors should be fully optional.
+        """
+        ns = config.NameScheme(fields={'subject': r'\d+'}, templates={})
+
+        assert ns.field_map == {}
+        assert ns.post_processors == []
+
+    def test_accepts_template_that_references_a_field(self):
+        """A template should be properly filled in when it references a field.
+        """
+        ns = config.NameScheme(
+            fields={'subject': r'\d+'},
+            templates={'sub_dir': 'sub-{subject}'},
+        )
+
+        assert ns.scheme['subject'] == r'(?P<subject>\d+)'
+        assert ns.scheme['sub_dir'] == r'(?P<sub_dir>sub-(?P<subject>\d+))'
+
+    def test_accepts_template_that_references_an_earlier_defined_template(
+            self
+    ):
+        """A template should be able to be filled in with another template.
+        """
+        ns = config.NameScheme(
+            fields={'subject': r'\d+', 'session': r'\d+'},
+            templates={
+                'sub_dir': 'sub-{subject}',
+                'full_dir': '{sub_dir}/ses-{session}',
+            },
+        )
+
+        expected_regex = (
+            r'(?P<full_dir>'
+            r'(?P<sub_dir>sub-(?P<subject>\d+))'
+            r'/ses-(?P<session>\d+))'
+        )
+
+        assert 'sub_dir' in ns.scheme
+        assert 'full_dir' in ns.scheme
+        assert ns.scheme['full_dir'] == expected_regex
+
+    def test_field_map_correctly_renames_field_group_names(self):
+        """A field name in field_map should have its group renamed.
+        """
+        ns = config.NameScheme(
+            field_map={'subject': 'sub'},
+            fields={'subject': r'\d+'},
+            templates={},
+        )
+
+        assert ns.scheme['subject'] == r'(?P<sub>\d+)'
+
+    def test_field_map_correctly_renames_template_group_names(self):
+        """A template name in field_map should have its group renamed.
+        """
+        ns = config.NameScheme(
+            field_map={'sub_dir': 'subject_dir'},
+            fields={'subject': r'\d+'},
+            templates={'sub_dir': 'sub-{subject}'},
+        )
+
+        assert ns.scheme['sub_dir'] == r'(?P<subject_dir>sub-(?P<subject>\d+))'
+
+    def test_scheme_property_matches_private_attr(self):
+        """The 'scheme' property should just return the private attribute.
+        """
+        ns = config.NameScheme(fields={'subject': r'\d+'}, templates={})
+
+        assert ns.scheme is ns._scheme  # pylint: disable=protected-access
+
+    def test_exception_raised_when_template_references_unknown_field(self):
+        """Template can't be filled if it requests an undefined field.
+        """
+        with pytest.raises(ValidationError, match='unknown field'):
+            config.NameScheme(
+                fields={'subject': r'\d+'},
+                templates={'sub_dir': 'sub-{nonexistent}'},
+            )
+
+    def test_exception_raised_when_template_referenced_before_it_exists(self):
+        """If a template uses another template, it must be defined after it.
+        """
+        with pytest.raises(ValidationError, match='must be defined after'):
+            config.NameScheme(
+                fields={'subject': r'\d+', 'session': r'\d+'},
+                templates={
+                    'full_dir': '{sub_dir}/ses-{session}',
+                    'sub_dir': 'sub-{subject}',
+                },
+            )
+
+    def test_template_with_no_references_is_valid(self):
+        """Accept templates that are literals or plain regexes.
+        """
+        ns = config.NameScheme(
+            fields={'subject': r'\d+'},
+            templates={'literal': 'just-a-literal-string'},
+        )
+
+        assert ns.scheme['literal'] == r'(?P<literal>just-a-literal-string)'
+
+    def test_multiple_unknown_fields_get_reported_at_once(self):
+        """If multiple errors exist at once, all should be reported in one go.
+        """
+        with pytest.raises(ValidationError) as exc_info:
+            config.NameScheme(
+                fields={'subject': r'\d+'},
+                templates={'bad': '{foo}-{bar}'},
+            )
+
+        msg = str(exc_info.value)
+        assert 'foo' in msg
+        assert 'bar' in msg
+
+    def test_duplicate_groups_in_a_template_raise_unique_exception(self):
+        """Raise when a template mistakenly duplicates a group name.
+        """
+        with pytest.raises(ValidationError, match='Duplicate fields exist'):
+            # Both fields have their groups renamed to 'dup'
+            # And then the template references both fields... leading to
+            # two regex groups named 'dup'
+            config.NameScheme(
+                field_map={'subject': 'dup', 'session': 'dup'},
+                fields={'subject': r'\d+', 'session': r'\d+'},
+                templates={'full_dir': 'sub-{subject}/ses-{session}'},
+            )
+
+    def test_bare_quantifier_raises_unique_exception(self):
+        """If the user gives a regex with something like '*' it should fail.
+        """
+        with pytest.raises(ValidationError, match='quantifiers'):
+            config.NameScheme(fields={'subject': '*'}, templates={})
+
+    def test_raises_exception_on_generic_regex_compile_issues(self):
+        """Any generic issue compiling a regex should raises an exception.
+        """
+        with pytest.raises(ValidationError, match='cannot compile'):
+            config.NameScheme(fields={'subject': '('}, templates={})
+
+    def test_collects_all_field_errors_when_multiple_exist(self):
+        """If multiple issues exist, it shouldn't just raise the first one.
+        """
+        with pytest.raises(ValidationError) as exc_info:
+            config.NameScheme(
+                fields={'subject': '*', 'session': '('},
+                templates={},
+            )
+
+        errors = exc_info.value.errors()
+        assert len(errors) == 2
